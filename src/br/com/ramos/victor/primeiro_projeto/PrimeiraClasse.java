88ca037afd8e30b4b5bbdecac28c7f89cf4500b4
Victor <victor.rmsp@gmail.com>
2024-08-10 01:36:25 -0300

    ajeitando diretórios para utilizar a IDE IntelliJ

--- src/br/com/ramos/victor/primeiro_projeto/PrimeiraClasse.java
+++ src/br/com/ramos/victor/primeiro_projeto/PrimeiraClasse.java
@@ -1,3 +1,9 @@
+/**
+ * @author victor.ramos
+ */
+
+package br.com.ramos.victor.primeiro_projeto;
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Hello Victor");
