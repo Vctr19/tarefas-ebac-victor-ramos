e59beba732d82731a7a81e4c6509f43c4da603ad
Victor <victor.rmsp@gmail.com>
2024-08-21 16:04:45 -0300

    comeÃcando implementacao do exercicio de listas

---
+++ src/br/com/ramos/victor/tarefa_colecoes/ListaNomes.java
@@ -0,0 +1,14 @@
+package br.com.ramos.victor.tarefa_colecoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaNomes {
+
+//      TODO: Objeto que percorre string e retornar uma lista de nomes em ordem alfabetica
+//   private List<String> listaNomes(String lista){
+
+        //List<String> listaNomes = new ArrayList<>(lista.split(","))
+
+//    }
+}
